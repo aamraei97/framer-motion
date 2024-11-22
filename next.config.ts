1eed12bc52d6cec36b1c57870029b4f59a01dfa0
Arman Amraei <aamraei97@gmail.com>
2024-11-22 14:52:38 +0330

    fix: add assetPrefix property to next.config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/framer-motion",
   // for static site that have base path we need to set assetPrefix; otherwise the assets will not be loaded
-  assetPrefix: "/framer-motion",
+  assetPrefix: "/framer-motion/",
   output: "export",
 };
 
